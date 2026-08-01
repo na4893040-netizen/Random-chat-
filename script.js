21f30f846e29b8a5d18db13dac76de712e91e2eb
na4893040-netizen <na4893040@gmail.com>
2026-08-01 22:23:59 +0500

    Implement basic chat functionality with socket.io

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const socket = io();
 
+function sendMessage() {
+  const input = document.getElementById("message");
+  const message = input.value;
+
+  if (message.trim() !== "") {
+    socket.emit("chat message", message);
+    input.value = "";
+  }
+}
+
+socket.on("chat message", function(msg) {
+  const chatBox = document.getElementById("chatBox");
+  chatBox.innerHTML += "<p>" + msg + "</p>";
+  chatBox.scrollTop = chatBox.scrollHeight;
+});
